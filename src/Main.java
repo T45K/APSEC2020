83e8faf488b0f3c0f90dcdff0cb2d27fe1e527a1
T45K <tasktas9@gmail.com>
2020-07-11 18:44:33 +0900

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    void fizzBuzz1(int i) {
+        if (i % 3 == 0) {
+            System.out.println("fizz");
+        } else if (i % 5 == 0) {
+            System.out.println("buzz");
+        } else if (i % 15 == 0) {
+            System.out.println("fizzbuzz");
+        }else {
+            System.out.println(i);
+        }
+    }
+
+    void fizzBuzz2(int j) {
+        if (j % 3 == 0) {
+            System.out.println("fizz");
+        } else if (j % 5 == 0) {
+            System.out.println("buzz");
+        } else if (j % 15 == 0) {
+            System.out.println("fizzbuzz");
+        }else {
+            System.out.println(j);
+        }
+    }
+}
